4cb9473a6019610025fa855c1231bab76ae78dae
Andres Ramirez de Alba <43384948+AndresRamirez0394@users.noreply.github.com>
2023-09-13 18:56:44 -0600

    Avances a base de datos

--- signup/views.py
+++ signup/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework import status, serializers
+from rest_framework.response import Response
+from .serializers import UserSerializer
 
+class UserRegistrationView(generics.CreateAPIView):
+    serializer_class = UserSerializer
 # Create your views here.
