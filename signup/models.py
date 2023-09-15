9915f4ca12057a14ef528d21eea3f0178871cc34
Andres Ramirez de Alba <43384948+AndresRamirez0394@users.noreply.github.com>
2023-09-14 19:30:30 -0600

    Database updates

--- signup/models.py
+++ signup/models.py
@@ -1,5 +1,5 @@
-from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.db import models
 
 class CustomUserManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
@@ -10,7 +10,7 @@ class CustomUserManager(BaseUserManager):
         user.set_password(password)
         user.save(using=self._db)
         return user
-    
+
     def create_superuser(self, email, password=None, **extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
@@ -33,6 +33,20 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['first_name', 'last_name']
+
+    # Define related names for groups and user_permissions
+    groups = models.ManyToManyField(
+        'auth.Group',
+        blank=True,
+        related_name='customuser_set',  # Change 'user_set' to 'customuser_set'
+        related_query_name='customuser'
+    )
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        blank=True,
+        related_name='customuser_set',  # Change 'user_set' to 'customuser_set'
+        related_query_name='customuser'
+    )
 
     def __str__(self):
         return self.email
