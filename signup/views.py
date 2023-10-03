64e5b4c64af8cebf3b94942d514693c417e0cbec
Mario Juárez Castillo <mariojc29@gmail.com>
2023-10-03 16:50:10 -0600

    Restart

--- signup/views.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import generics
-from rest_framework import status, serializers
-from rest_framework.response import Response
-from .serializers import UserSerializer
-
-class UserRegistrationView(generics.CreateAPIView):
-    serializer_class = UserSerializer
-# Create your views here.
