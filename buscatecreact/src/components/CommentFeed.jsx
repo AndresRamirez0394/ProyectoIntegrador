d19193d5b16c13cc1c2d8bbb6015d942cee0004c
Gilberto Berttolini <85074402+Zathsiri@users.noreply.github.com>
2023-11-17 03:45:29 -0600

    CommentFeed

---
+++ buscatecreact/src/components/CommentFeed.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import { Box, Typography, Divider } from "@mui/material";
+import Comment from "./NewCommentComment";
+
+function CommentFeed({ comments }) {
+  const [loading, setLoading] = useState(true);
+
+  // Simulamos una carga inicial que se completa después de 2 segundos
+  setTimeout(() => {
+    setLoading(false);
+  }, 2000);
+
+  return (
+    <Box flex={4} p={{ xs: 0, md: 2 }}>
+      {loading ? (
+        // Muestra un esqueleto o indicador de carga mientras se cargan los comentarios
+        <Typography variant="h6">Loading Comments...</Typography>
+      ) : comments?.length === 0 ? (
+        // Muestra un mensaje si no hay comentarios
+        <Typography variant="body1">No comments yet.</Typography>
+      ) : (
+        // Muestra la lista de comentarios
+        comments.map((comment) => (
+          <React.Fragment key={comment.id}>
+            <Comment comment={comment} />
+            <Divider />
+          </React.Fragment>
+        ))
+      )}
+    </Box>
+  );
+}
+
+export default CommentFeed;
